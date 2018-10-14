ce9851d24f10bc5e69096af2a49ba3d9fb121b9d
yOriowm <42407691+yOriowm@users.noreply.github.com>
2018-10-14 16:16:19 -0300

    Update minimap.user.js

--- minimap.user.js
+++ minimap.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Pixelzone Minimap
-// @namespace    http://tampermonkey.net/
-// @version      1.1
-// @description  Pixelzone Minimap
+// @name         yOriowm Minimap
+// @namespace    yOriowm Minimap
+// @version      1.0
+// @description  yOriowm Minimap
 // @author       yOriowm#1046
 // @match        https://pixelzone.io/*
 // @match        http://pixelzone.io/*
@@ -17,14 +17,12 @@ Number.prototype.between = function(a, b) {
     max = Math.max.apply(Math, [a, b]);
   return this > min && this < max;
 };
-var range = 25;
-window.baseTepmlateUrl = 'https://raw.githubusercontent.com/yOriowm/OrionMinimap/master/';
+
+window.baseTepmlateUrl = 'https://raw.githubusercontent.com/yOriowm/OrionMinimap/master';
 
 window.addEventListener('load', function () {
-    //Regular Expression to get coordinates out of URL
-    re = /(.*)\/\?p=(\-?(?:\d*)),(\-?(?:\d*))/g;
     //Regular Expression to get coordinates from cursor
-    rec = /x\:(\d*) y\:(\d*)/g;
+    rec = /x\:(-?\d*) y\:(-?\d*)/g;
     gameWindow = document.getElementById("gameWindow");
     //DOM element of the displayed X, Y variables
     coorDOM = null;
@@ -52,20 +50,23 @@ window.addEventListener('load', function () {
     //Cachebreaker to force refresh
     cachebreaker = null;
 
+	vers = "OrionMinimap";
+
     var div = document.createElement('div');
     div.setAttribute('class', 'post block bc2');
-    div.innerHTML = '<div id="minimapbg" style="position: absolute; right: 1em; bottom: 1em;">' +
-        '<div class="posy" id="posyt" style="background-color: rgba(0, 0, 0, 0.75); color: rgb(250, 250, 250); text-align: center; line-height: 42px; vertical-align: middle; width: auto; height: auto; border-radius: 21px; padding: 6px;">' +
+    div.innerHTML = '<div id="minimapbg" style="position: absolute; right: 0.6em; bottom: 0.6em; z-index: 2;">' +
+        '<div class="posy" id="posyt" style="background-size: 100%; background-image: url(https://i.imgur.com/Hh3BTPx.png); color: rgb(255, 255, 255); text-align: center; line-height: 42px; vertical-align: middle; width: auto; height: auto; border-radius: 12px; padding: 10px;">' +
         '<div id="minimap-text" style="display: none;"></div>' +
-        '<div id="minimap-box" style="position: relative;width:420px;height:300px">' +
+        '<div id="minimap-box" style="position: relative;width:400px;height:300px">' +
         '<canvas id="minimap" style="width: 100%; height: 100%;z-index:1;position:absolute;top:0;left:0;"></canvas>' +
         '<canvas id="minimap-board" style="width: 100%; height: 100%;z-index:2;position:absolute;top:0;left:0;"></canvas>' +
         '<canvas id="minimap-cursor" style="width: 100%; height: 100%;z-index:3;position:absolute;top:0;left:0;"></canvas>' +
         '</div><div id="minimap-config" style="line-height:20px;">' +
-        '<span id="hide-map" style="cursor:pointer;">Hide minimap' +
-        '</span> | <span id="follow-mouse" style="cursor:pointer;"Follow mous' +
-        '</span> | Zoom: <span id="zoom-plus" style="cursor:pointer;font-weight:bold;">+</span>  /  ' +
-        '<span id="zoom-minus" style="cursor:pointer;font-weight:bold;">-</span>' +
+        '<link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet"> <span id="hide-map" style="cursor:pointer;color:white">Minimizar' +
+        '</span> | <span id="follow-mouse" style="cursor:pointer;">Seguir o mouse' +
+        '</span> | Zoom: <span id="zoom-plus" style="cursor:pointer;font-weight:bold;">▲</span>' +
+        '<span id="zoom-minus" style="cursor:pointer;font-weight:bold;">▼</span>' +
+        ' | <link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet"> </i><br />' + vers +
         '</div>' +
         '</div>';
     document.body.appendChild(div);
@@ -97,7 +98,7 @@ window.addEventListener('load', function () {
         document.getElementById("minimap-box").style.display = "none";
         document.getElementById("minimap-config").style.display = "none";
         document.getElementById("minimap-text").style.display = "block";
-        document.getElementById("minimap-text").innerHTML = "Haritayı Göster";
+        document.getElementById("minimap-text").innerHTML = "Expandir Mapa";
         document.getElementById("minimap-text").style.cursor = "pointer";
     };
     document.getElementById("minimap-text").onclick = function () {
@@ -129,13 +130,13 @@ window.addEventListener('load', function () {
     document.getElementById("follow-mouse").onclick = function () {
         toggle_follow = !toggle_follow;
         if (toggle_follow) {
-            this.innerHTML = "Fareyi Takip Et";
+            this.innerHTML = "Seguir a tela";
             loadTemplates();
             x_window = x;
             y_window = y;
             drawCursor();
         } else {
-            this.innerHTML = "Ekranı Takip Et";
+            this.innerHTML = "Seguir o mouse";
             getCenter();
         }
     };
@@ -161,8 +162,9 @@ window.addEventListener('load', function () {
                 x_window = x;
                 y_window = y;
             } else {
-                drawCursor();
+				getCenter();
             }
+			drawCursor();
             loadTemplates();
         }
     }, false);
@@ -176,7 +178,7 @@ function updateloop() {
     console.log("Updating Template List");
     // Get JSON of available templates
     var xmlhttp = new XMLHttpRequest();
-    var url = window.baseTepmlateUrl + "/templates/data.json?" + new Date().getTime();
+    var url = window.baseTepmlateUrl + "/templates/data.json";
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             template_list = JSON.parse(this.responseText);
@@ -207,7 +209,7 @@ function toggleShow() {
         document.getElementById("minimap-box").style.display = "none";
         document.getElementById("minimap-config").style.display = "none";
         document.getElementById("minimap-text").style.display = "block";
-        document.getElementById("minimap-text").innerHTML = "Show Minimap";
+        document.getElementById("minimap-text").innerHTML = "Expandir Mapa";
         document.getElementById("minimapbg").onclick = function () {
             toggleShow()
         };
@@ -268,11 +270,11 @@ function loadTemplates() {
         var temp_y = parseInt(template_list[template]["y"]) * 1;
         var temp_xr = parseInt(template_list[template]["x"]) + parseInt(template_list[template]["width"]);
         var temp_yb = parseInt(template_list[template]["y"]) + parseInt(template_list[template]["height"]);
-        // if (temp_xr <= x_left || temp_yb <= y_top || temp_x >= x_right || temp_y >= y_bottom)
-        //    continue
-        if (!x_window.between(temp_x-range*1, temp_xr+range*1))
+
+         if (temp_xr <= x_left || temp_yb <= y_top || temp_x >= x_right || temp_y >= y_bottom)
             continue
-        if (!y_window.between(temp_y-range*1, temp_yb+range*1))
+		console.log(x_window + ", " + y_window);
+        if (!x_window.between(temp_x, temp_xr) && !y_window.between(temp_y, temp_yb))
             continue
         console.log("Template " + template + " is in range!");
         // console.log(x_window, y_window);
@@ -282,7 +284,7 @@ function loadTemplates() {
         if (zooming_in == false && zooming_out == false) {
             document.getElementById("minimap-box").style.display = "none";
             document.getElementById("minimap-text").style.display = "block";
-            document.getElementById("minimap-text").innerHTML = "No template nearby.";
+            document.getElementById("minimap-text").innerHTML = "Não há templates aqui";
         }
     } else {
         document.getElementById("minimap-box").style.display = "block";
@@ -378,8 +380,8 @@ function drawCursor() {
 
 function getCenter() {
     var url = window.location.href;
-    x_window = url.replace(re, '$2');
-    y_window = url.replace(re, '$3');
+    x_window = +url.split("?p=")[1].split(",")[0];
+    y_window = ++url.split("?p=")[1].split(",")[1];
     if (x_window == url || y_window == url) {
         x_window = 0;
         y_window = 0;
